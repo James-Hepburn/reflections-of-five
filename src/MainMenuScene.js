9a1829fb4768dee1afe5e384a85bb1e19620cbb2
James Hepburn <jhepb059@uottawa.ca>
2025-11-04 14:07:47 -0700

    Main menu layout

---
+++ src/MainMenuScene.js
@@ -0,0 +1,63 @@
+import Phaser from 'phaser';
+
+export default class MainMenuScene extends Phaser.Scene {
+  constructor() {
+    super('MainMenu');
+  }
+
+  preload() {
+    this.load.image('background', 'assets/images/menu_bg.png');
+  }
+
+  create() {
+    const { width, height } = this.scale;
+
+    const bg = this.add.image(width / 2, height / 2, 'background')
+      .setScale(4) 
+      .setAlpha(0.7);
+
+    this.tweens.add({
+      targets: bg,
+      alpha: { from: 0, to: 0.7 },
+      duration: 2000,
+      ease: 'Sine.easeInOut'
+    });
+
+    const title = this.add.text(width / 2, height / 3, 'Reflections of Five', {
+      fontFamily: 'serif',
+      fontSize: '48px',
+      color: '#ffffff'
+    }).setOrigin(0.5);
+
+    this.tweens.add({
+      targets: title,
+      alpha: { from: 0, to: 1 },
+      duration: 2000,
+      ease: 'Sine.easeInOut'
+    });
+
+    const newGameText = this.add.text(width / 2, height / 2, 'New Game', {
+      fontFamily: 'serif',
+      fontSize: '28px',
+      color: '#cccccc'
+    }).setOrigin(0.5).setInteractive();
+
+    newGameText.on('pointerover', () => newGameText.setColor('#ffffff'));
+    newGameText.on('pointerout', () => newGameText.setColor('#cccccc'));
+    newGameText.on('pointerdown', () => {
+      console.log('Start game'); 
+    });
+
+    const continueText = this.add.text(width / 2, height / 2 + 60, 'Continue', {
+      fontFamily: 'serif',
+      fontSize: '28px',
+      color: '#666666'
+    }).setOrigin(0.5).setInteractive();
+
+    continueText.on('pointerover', () => continueText.setColor('#888888'));
+    continueText.on('pointerout', () => continueText.setColor('#666666'));
+    continueText.on('pointerdown', () => {
+      console.log('Continue game'); 
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,24 +1,21 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import Phaser from 'phaser';
+import MainMenuScene from './MainMenuScene.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const config = {
+  type: Phaser.AUTO,
+  backgroundColor: '#000000', 
+  pixelArt: true, 
+  scale: {
+    mode: Phaser.Scale.FIT,           
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 1280,                      
+    height: 720
+  },
+  scene: [MainMenuScene]
+};
 
-setupCounter(document.querySelector('#counter'))
+const game = new Phaser.Game(config);
+
+window.addEventListener('resize', () => {
+  game.scale.resize(window.innerWidth, window.innerHeight);
+});
